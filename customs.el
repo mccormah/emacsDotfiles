e52e6d6fc9fddd5d5f69578d8ad0ce3cfaa50098
Franklin McCormack <franklin.mccormack@gmail.com>
2024-12-14 19:32:13 -0500

    Updated my emacs configuration by adding a packages Rainbow Delimiters and General. I also customized emacs (customs.el) by removing the scroll bar.

--- customs.el
+++ customs.el
@@ -1,6 +1,10 @@
-(set-cursor-color "#CB0005")
+(set-cursor-color "#CB0005") ;; Sets teh cursor color to Warhorse Red.
+(delete-selection-mode 1) ;; You can select text and delete it by typing.
+(electric-indent-mode -1) ;; Turn off the weird indenting that Emacs does by default.
+(scroll-bar-mode -1) ;; Disable the scroll bar
 
 (provide 'customs)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
