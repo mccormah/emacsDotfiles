6a9f0b72c19192281881d0e91aa1b1584c1aa4eb
Franklin McCormack <franklin.mccormack@gmail.com>
2024-12-19 19:43:37 -0500

    Updated my emacs config file by adding the WHICH-KEY package.

--- config.el
+++ config.el
@@ -512,3 +512,23 @@
 
 (use-package transient
   :ensure t)
+
+(use-package which-key
+  :ensure t
+  :init
+  (which-key-mode 1)
+  :diminish
+  :config
+  (setq which-key-side-window-location 'bottom
+        which-key-sort-order #'which-key-key-order-alpha
+        which-key-allow-imprecise-window-fit nil
+        which-key-sort-uppercase-first nil
+        which-key-add-column-padding 1
+        which-key-max-display-columns nil
+        which-key-min-display-lines 6
+        which-key-side-window-slot -10
+        which-key-side-window-max-height 0.25
+        which-key-idle-delay 0.8
+        which-key-max-description-length 25
+        which-key-allow-imprecise-window-fit nil
+        which-key-separator " → " ))
