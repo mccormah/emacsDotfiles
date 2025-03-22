87cb02000d4ad5d714c124306eb29fc7bdfaa038
Franklin McCormack <franklin.mccormack@gmail.com>
2025-03-22 16:12:34 -0400

    Updated the theme to zoom-palenight

--- config.el
+++ config.el
@@ -586,8 +586,8 @@
   :config
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  ;; Sets the default theme to load!!! 
-  (load-theme 'doom-one t)
+  ;; Sets the default theme to load! I prefer doom-one or doom-palenight.
+  (load-theme 'doom-palenight t)
   ;; Enable custom neotree theme (all-the-icons must be installed!)
   (doom-themes-neotree-config)
   ;; Corrects (and improves) org-mode's native fontification.
