86cb3909c86995b50d9babce753edbf85e916b24
Franklin McCormack <franklin.mccormack@gmail.com>
2026-08-22 21:48:30 -0400

    Added comments to the init.el file

--- init.el
+++ init.el
@@ -1,4 +1,14 @@
+;;; init.el --- Init File -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;;; Emacs configuarion file (config.org)
+;;; loaded via org-babel-load-file
+
+;;; Code:
+
 (org-babel-load-file
  (expand-file-name
   "config.org"
   user-emacs-directory))
+
+;;; init.el ends here
